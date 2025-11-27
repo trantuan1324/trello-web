db8627de726ed4d295a046468bee2d5dbe6c1bf5
Rabbyte <quangtuantran1324@gmail.com>
2025-11-27 11:07:44 +0700

    config prettier file

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import App from "./App.jsx";
-import CssBaseline from "@mui/material/CssBaseline";
-import theme from "./theme";
-import ReactDOM from "react-dom/client";
-import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
+import React from 'react';
+import App from './App.jsx';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+import ReactDOM from 'react-dom/client';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
     </CssVarsProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
