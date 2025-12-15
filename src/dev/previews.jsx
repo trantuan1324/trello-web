2f03058b3f1c5e30363cda165d32fd6229388a13
Rabbyte <quangtuantran1324@gmail.com>
2025-12-15 17:28:36 +0700

    feat: drag and drop columns

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -1,12 +1,16 @@
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import Board from "../pages/Boards/_id.jsx";
+import BoardContent from "~/pages/Boards/BoardContent/BoardContent.jsx";
 
 const ComponentPreviews = () => {
   return (
     <Previews palette={<PaletteTree/>}>
       <ComponentPreview path="/Board">
         <Board/>
+      </ComponentPreview>
+      <ComponentPreview path="/BoardContent">
+        <BoardContent/>
       </ComponentPreview>
     </Previews>
   )
