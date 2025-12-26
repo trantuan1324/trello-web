202a860ddde7c0360a4145411d0fb00cd43f72c5
Rabbyte <quangtuantran1324@gmail.com>
2025-12-26 16:09:32 +0700

    dev: drag overlay card and column

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/Card/Card.jsx
@@ -8,6 +8,8 @@ import {
 } from '@mui/material'
 import { Attachment, Comment, Group } from '@mui/icons-material'
 import React from 'react'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
 export default function Card({ card }) {
   const shouldShowCardAction = () => {
@@ -17,8 +19,32 @@ export default function Card({ card }) {
       card?.attachments?.length
     )
   }
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging
+  } = useSortable({ id: card._id, data: { ...card } })
+  const dndKitCardStyle = {
+    // nếu dùng CSS.Tranform thì khi kéo thả phần tử sẽ bị stretch
+    // https://github.com/clauderic/dnd-kit/issues/117
+    transform: CSS.Translate.toString(transform),
+    transition,
+    opacity: isDragging ? 0.5 : undefined,
+    border: isDragging ? '1px solid #2ecc71' : undefined
+  }
+
   return (
-    <MuiCard sx={{ cursor: 'pointer', overflow: 'unset' }}>
+    <MuiCard
+      ref={setNodeRef}
+      style={dndKitCardStyle}
+      {...attributes}
+      {...listeners}
+      sx={{ cursor: 'pointer', overflow: 'unset' }}
+    >
       {card?.cover && <CardMedia sx={{ height: 140 }} image={card?.cover} />}
 
       <CardContent sx={{ p: 1.5, '&:last-child': { p: 1.5 } }}>
