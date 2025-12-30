3b0251fbfacf10dedaea4a328cb8a52da4826513
Rabbyte <quangtuantran1324@gmail.com>
2025-12-30 23:24:27 +0700

    fix: flickering when drag and drop card

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import App from './App.jsx';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme';
-import ReactDOM from 'react-dom/client';
-import {Experimental_CssVarsProvider as CssVarsProvider} from '@mui/material/styles';
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
+import React from 'react'
+import App from './App.jsx'
+import CssBaseline from '@mui/material/CssBaseline'
+import theme from './theme'
+import ReactDOM from 'react-dom/client'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
+import { DevSupport } from '@react-buddy/ide-toolbox'
+import { ComponentPreviews, useInitial } from './dev/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline/>
-      <DevSupport ComponentPreviews={ComponentPreviews}
-                  useInitialHook={useInitial}
-      >
-        <App/>
-      </DevSupport>
-    </CssVarsProvider>
-  </React.StrictMode>
-);
+  <CssVarsProvider theme={theme}>
+    <CssBaseline />
+    <DevSupport
+      ComponentPreviews={ComponentPreviews}
+      useInitialHook={useInitial}
+    >
+      <App />
+    </DevSupport>
+  </CssVarsProvider>
+)
